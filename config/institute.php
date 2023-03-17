1667ec7ddd13cd7ad1e89141391fbd925f122e46
ckpcet <web@ckpcet.ac.in>
2023-03-17 18:47:26 +0530

    institute info updated

--- config/institute.php
+++ config/institute.php
@@ -19,23 +19,21 @@ return [
   'info' => [
     "id" => 102,
     "code" => "56ea70e8-0746-405e-b77a-5ea8a4d2d925",
-    "name" => "C. K. Pithawala Institute of Pharmaceutical Science & Research",
+    "name" => "C. K. Pithawalla Institute of Pharmaceutical Science & Research",
     "short_name" => "CKPIPSR",
-    "vision" => "Committed to promote high quality education, training and research in pharmacy to meet the needs of tomorrow’s healthy society.",
+    "vision" => "TO DEVELOP PHARMACY GRADUATES WITH FUNDAMENTAL KNOWLEDGE AND PROFESSIONAL COMPETENCE TO IMPROVE HEALTHCARE NEED OF COMMUNITY AND INDUSTRY.",
     "mission" => [
-      "M1" => "To create an arena of the highest quality professional pharmacy education at all levels, to produce competent pharmacists with innovative skills",
-      "M2" => "To establish Continuing Professional Development (CPD) program in the institution for practicing professionals to undertake the national task of education and research.",
-      "M3" => "To strengthen the industry – institution interaction to explore, substantiate and integrate native medicinal knowledge with modern pharmaceutical technology.",
-      "M4" => "To create a model pharmacy in the institution to strengthen the relation between public and pharmacy.",
-      "M5" => "To provide excellent work environment and to promote technical Superiority.",
-      "M6" => "To collaborate with national and international organizations for Outstanding educational / services / research programs."
+      "M1" => "To provide state of art teaching learning process.",
+      "M2" => "To train students as technical and knowledgeable workforce for society.",
+      "M3" => "To train students as technical and knowledgeable workforce for society."
     ],
     "description" => "C.K Pithawalla Institute Of Pharmaceutical Science & Research was founded in 2005 and is being managed by the Navyug Vidyabhavan Trust which was founded in February, 1965.\r\n\r\nWithin a short span of its operation, the college has provided all the necessary facilities to help the students which includes a well eastablished library containing a large number of books, reference books, technical & general magazines.\r\n\r\nHonourable late Shri C.K. Pithawalla has been the driving force for the setting up of this college named after him and is continually drawing inspiration from him. This institute of learning in particular, like many others, owes a great deal to his vision, understanding and missionary zeal right from its inception to its bloassoming.",
     "address" => "Opposite Surat Airport,\r\nBehind DPS School,\r\nNear Malvan Mandir,\r\nDumas Road, Surat - 395007.",
     "email" => "ckpipsr@gmail.com",
     "mobile" => "6355065636",
     "other_emails" => [
-      "ckpipsrprincipal@gmail.com"
+      "contact@ckpipsr.ac.in",
+      "principal@ckpipsr.ac.in"
     ],
     "other_contact_nos" => [
       "9099063116"
@@ -48,7 +46,7 @@ return [
     "instagram" => "https://www.instagram.com/ckpipsr",
     "telegram" => "https://t.me/joinchat/aBJZdVV41Sw5M2U1",
     "whatsapp" => "https://api.whatsapp.com/send?phone=919427474602",
-    "youtube" => null,
+    "youtube" => "https://www.youtube.com/channel/UCkk5Xd_bjJkIDirh7HpBNhg",
     "website" => "https://ckpipsr.ac.in",
     "logo" => "https://console.navyugtrust.org/institute/102/logo",
   ]
