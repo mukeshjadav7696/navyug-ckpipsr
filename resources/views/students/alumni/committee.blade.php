4c1cf99addf871265a71c5c071e395ef6c34365d
ckpcet <web@ckpcet.ac.in>
2023-03-18 14:10:32 +0530

    student > alumni

--- resources/views/students/alumni/committee.blade.php
+++ resources/views/students/alumni/committee.blade.php
@@ -1,48 +1,18 @@
-<div class="columns">
-    <div class="column">
-        <div class="box">
-            <span class="tag is-large is-primary">Managing Committee</span>
-            <table class="table is-striped is-fullwidth">
-                <thead>
-                    <tr>
-                        <th>Role</th>
-                        <th>Name of the Member</th>
-                        <th>Designation</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($committeeM as $member)
-                        <tr>
-                            <td>{{ $member['role'] }}</td>
-                            <td>{{ $member['name'] }}</td>
-                            <td>{{ $member['designation'] }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    </div>
-    <div class="column">
-        <div class="box">
-            <span class="tag is-large is-primary">Executive Committee</span>
-            <table class="table is-striped is-fullwidth">
-                <thead>
-                    <tr>
-                        <th>Role</th>
-                        <th>Name of the Member</th>
-                        <th>Designation</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($committeeE as $member)
-                        <tr>
-                            <td>{{ $member['role'] }}</td>
-                            <td>{{ $member['name'] }}</td>
-                            <td>{{ $member['designation'] }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>
+<table class="table is-striped is-fullwidth">
+    <thead>
+        <tr>
+            <th>Role</th>
+            <th>Name of the Member</th>
+            <th>Designation</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($committee as $member)
+            <tr>
+                <td>{{ $member['role'] }}</td>
+                <td>{{ $member['name'] }}</td>
+                <td>{{ $member['designation'] }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
