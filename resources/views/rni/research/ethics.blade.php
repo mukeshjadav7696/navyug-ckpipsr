b6b2d5b4d803b42b3b1bbc462ea50d442bc9e27e
Yogesh Kapuriya <ymkapuriya@gmail.com>
2023-08-03 15:20:06 +0530

    ethics and publication updated

--- resources/views/rni/research/ethics.blade.php
+++ resources/views/rni/research/ethics.blade.php
@@ -2,7 +2,81 @@
     <x-slot name="content">
         <x-site.pagetitle title="Research Ethics" />
         <x-page-content>
-            <x-comming-soon />
+            <div class="tab-content" id="tab-about">
+                <div class="box">
+                    <div class="content has-text-justified">
+                       <h5>Code of Ethics for Research</h5>
+                            <p>Following code of Ethics for Research provides major benefits to research organisations, researcher
+                                    and general public. It helps to conduct research of the highest quality and standards.
+                            </p>
+                               
+                        <h5>Code of Ethics for Organization</h5>                                
+                            <ol>
+                                <li>
+                                    Shall strive to disseminate work of the highest quality and excellence while conducting research.
+                                </li>
+                                <li>
+                                    Shall ensure the dignity, rights, safety and wellbeing of all involved in research and avoid
+                                    unreasonable risk or harm to research subjects, patients, participants, researchers and others.
+                                </li>
+                                <li>
+                                    Should ensure that all research is carried out keeping in mind the health, safety legislation
+                                    and good practice.
+                                </li>
+                                <li>
+                                    Shall ensure that good practice forms an important and integral part of research.
+                                </li>
+                                <li>
+                                    Shall establish clear policies and procedures that covering the principles of good practice in
+                                    research and make researchers aware of these policies and procedures
+                                </li>
+                            </ol>                                
+                        <h5>Code of Ethics for Researcher:</h5>
+                            <ul>
+                                <li>
+                                    Shall promote the open exchange of ideas, research methods, data and results.
+                                </li>
+                                <li>
+                                    Misconduct in academic research such as plagiarism, piracy, abuse of intellectual property
+                                    and research resource, defamation, misinterpretation, fabrication and fraud must be avoided
+                                    in carrying out research.
+                                </li>
+                                <li>
+                                    Shall not try to sabotage others from completing their work by either damaging or
+                                    disrupting data or experiments of others; or wilfully failing to observe their terms and
+                                    conditions.
+                                </li>
+                                <li>
+                                    Shall realize that by their work they are accountable to the general public and should act
+                                    accordingly.
+                                </li>
+                                    Researcher must comply with all legal and ethical requirements and other guidelines that
+                                    apply to their research and must submit research proposals for review before ethics
+                                    committee where appropriate and abide by the outcome of that review.
+                                <li>
+                                    Shall not expose the animals or subjects to unnecessary harm when conducting studies and
+                                    must try to anticipate any risks that the proposed research might produce to them.
+                                </li>
+                                <li>
+                                    Shall make research designs available to peer reviewers and journal editors when submitting
+                                    research reports for publication.
+                                </li>
+                                <li>
+                                    Shall try to identify conflict of interest, and must declared and addressed in order to avoid
+                                    poor practice in research.
+                                </li>
+                                <li>
+                                    Shall consider the opportunities for reduction, replacement and refinement of involving
+                                    animals in research projects.
+                                </li>
+                                <li>
+                                    Researchers should ensure that the terms and conditions of any grant or contract related to
+                                    the research are adhered to.
+                                </li>
+                            </ul>     
+                    </div>
+                </div>
+            </div>
         </x-page-content>
     </x-slot>
 
--- resources/views/rni/research/publications.blade.php
+++ resources/views/rni/research/publications.blade.php
@@ -2,7 +2,25 @@
     <x-slot name="content">
         <x-site.pagetitle title="Research Publications" />
         <x-page-content>
-            <x-comming-soon />
+            <div class="tab-content" id="tab-about">
+                <div class="box">
+                    <div class="content has-text-justified">
+                        <div class="section">
+                            <span class="tag is-info is-large">Publications</span>
+                            <hr />
+                            <div class="list">
+                                <div class="list-item">
+                                    <div class="list-item-content">
+                                        <div class="list-item-title">
+                                            <a href=" https://console-navyugtrust-org.s3.ap-south-1.amazonaws.com/app/institutes/102/departments/docs/64c1fc24f3171.pdf" target="_blank">Research Publication AY 2022-2023</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </x-page-content>
     </x-slot>
 
