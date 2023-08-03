ebf08ed489983dbe53e3df994f50b45310946e98
Yogesh Kapuriya <ymkapuriya@gmail.com>
2023-08-03 14:32:05 +0530

    Popup updated

--- resources/views/home/popup.blade.php
+++ resources/views/home/popup.blade.php
@@ -2,8 +2,13 @@
   <div class="modal is-active" id="modal-popup">
     <div class="modal-background"></div>
     <div class="modal-content">
-      <p class="image">
+      <p class="image" >
         <img src="{{ asset("/images/Mpharm_2.jpg") }}" />
+      </p>
+    </div>
+    <div class="buttons is-centered">
+      <p class="content pt-3">
+      <a href="https://forms.gle/yQKPF5f32fXiNNAe7" target="_blank" class="button"> Click here for Inquiry Form</a>
       </p>
     </div>
     <button class="modal-close is-large" aria-label="close"></button>
